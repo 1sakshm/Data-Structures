ded221851e7d7ce8c21889a344e351f9523394e6
1sakahm <sakshamsai26@gmail.com>
2025-05-20 00:48:00 +0530

    find added

---
+++ LinkedList/Find.java
@@ -0,0 +1,13 @@
+public class Find {
+    ListNode head;
+    public boolean find(int value){
+        ListNode current = head;
+        while(current != null){
+            if(current.data == value){
+                return true;
+            }
+            current = current.next;
+        }
+        return false;
+    }
+}
